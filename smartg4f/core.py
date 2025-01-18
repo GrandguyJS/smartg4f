a5728a43061830f6ac8b6486b56ca59a0fe876df
GrandguyJS <GrandguyMC@gmail.com>
2025-01-18 17:17:22 +0100

    added logging functionality

--- smartg4f/core.py
+++ smartg4f/core.py
@@ -13,13 +13,20 @@ def default_validate(param):
     except:
         return False
 
-async def get_fastest_providers(prompt, validation, model, timeout):
-    providers = [provider for provider in g4f.Provider.__providers__ if provider.working]
+async def get_fastest_providers(prompt, validation, model, timeout, log):
+    def log_message(*args):
+        if log:
+            print(*args)
+        else:
+            pass
+
+    providers = [provider for provider in g4f.Provider.__providers__ if provider.working and not provider.needs_auth]
 
     working_providers = []
 
     for provider in providers:
         try:
+            log_message("Testing ", provider.__name__, "...")
             async with asyncio.timeout(timeout):
                 res = await g4f.ChatCompletion.create_async(
                     model=model,
@@ -29,8 +36,9 @@ async def get_fastest_providers(prompt, validation, model, timeout):
                 if not validation(res):
                     raise
             working_providers.append(provider)
+            log_message(provider.__name__, " working")
         except:
-            pass
+            log_message(provider.__name__, " not working")
 
     if len(working_providers) == 0:
         print("Every provider failed!")
@@ -40,8 +48,8 @@ async def get_fastest_providers(prompt, validation, model, timeout):
     # Return a list of all working providers for RetryProviders
     return working_providers
 
-async def get_provider(prompt="Say hello", validation=default_validate, model="gpt-4o", timeout=15):
+async def get_provider(prompt="Say hello", validation=default_validate, model="gpt-4o", timeout=15, log=False):
     # Returns providers
     # Use provider=get_provider()
-    providers = await get_fastest_providers(prompt=prompt, validation=validation, model=model, timeout=timeout)
+    providers = await get_fastest_providers(prompt=prompt, validation=validation, model=model, timeout=timeout, log=log)
     return RetryProvider(providers, shuffle=False, single_provider_retry=False, max_retries=1)
